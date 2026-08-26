97c99a48d6fd65165b4eff9e0512582c416ac765
Gabbe17 <gabriel.trpkoski@student.chasacademy.se>
2026-08-26 18:49:22 +0200

    Refactor print statements into variables

--- intro.py
+++ intro.py
@@ -1,5 +1,11 @@
-print("Hej ITSX26!")
-print("Jag studerar IT- och cybersäkerhet.")
+hälsning = "Hej ITSX26!"
+intro = "Jag studerar IT- och cybersäkerhet."
+
+#print("Hej ITSX26!")
+#print("Jag studerar IT- och cybersäkerhet.")
+
+print(hälsning)
+print(intro)
 
 namn = input("Vad heter du? ")
 print(f"Välkommen, {namn}! Kul att du studerar IT- och cybersäkerhet.")
